56de5e830f64071e43606e2d981bc2077aa066f6
mortezashoghi <morteza.shoghi@gmail.com>
2021-03-12 13:31:15 +0330

    add KeyboardAvoidingView and add slick slider

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -6,7 +6,7 @@ import {
   TouchableHighlight,
   Linking,
   navigation,
-  Switch,
+  Switch,KeyboardAvoidingView
 } from "react-native";
 import { Button } from "react-native-paper";
 import { TextInput } from "react-native-paper";
@@ -14,7 +14,8 @@ import { bold } from "ansi-colors";
 import Chart from "./Chart";
 import { connect } from "react-redux";
 import { registerNewUser } from "../redux";
-
+import { Directions } from "react-native-gesture-handler";
+import Production from './production';
 const Home = (props) => {
   const [mobile, setMobile] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -22,93 +23,100 @@ const Home = (props) => {
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
-    <View style={(styles.maincontainer, { flexDirection: "column" })}>
-      <View style={(styles.maincontainer, { flexDirection: "row" })}>
-        <TouchableHighlight
-          style={[styles.innerdivs, { backgroundColor: "pink" }]}
-          onPress={() => {}}
-        >
-          <View style={styles.button}>
-            <Text style={styles.textstyle}>کاربران</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={[styles.innerdivs, { backgroundColor: "#00FFFF" }]}
-          onPress={() => {
-            //navigation.navigate("About");
-            props.navigation.navigate("About");
-          }}
-        >
-          <View style={styles.button}>
-            <Text style={styles.textstyle}>خدمات</Text>
-          </View>
-        </TouchableHighlight>
+    <KeyboardAvoidingView
+    behavior={Platform.OS === "ios" ? "padding" : "height"}
+    style={styles.container}
+  >
+    <View style={styles.container}>
+      <View style={{flex:1, backgroundColor: 'powderblue'}} >
+         <View style={(styles.maincontainer, { flexDirection: "row",height:"100%" })}>
+                <TouchableHighlight
+                   style={[styles.innerdivs, { backgroundColor: "#ea8e38" }]}
+                   onPress={() => {
+                     props.navigation.navigate("About");
+                   }}
+                 >
+                       <Text style={styles.textstyle}>سان شاین</Text>
+                 </TouchableHighlight>
+                 <TouchableHighlight
+                   style={[styles.innerdivs, { backgroundColor: "#00FFFF" }]}
+                   onPress={() => {
+                     props.navigation.navigate("About");
+                   }}
+                 >
+                       <Text style={styles.textstyle}>خدمات</Text>
+                 </TouchableHighlight>
+                </View>
+      </View>
+    
+      <View style={{flex:2, backgroundColor: 'steelblue'}} >
+         <Production />
       </View>
 
-      <View>
-        <TextInput
-          type="flat"
-          style={styles.inputStyle}
-          label="موبایل"
-          value={mobile}
-          onChangeText={(mobile) => setMobile(mobile)}
-          placeholder="شماره موبایل"
-          keyboardType={"numeric"}
-        ></TextInput>
-        <TextInput
-          type="flat"
-          style={styles.inputStyle}
-          label="ایمیل"
-          value={email}
-          onChangeText={(email) => setEmail(email)}
-          placeholder="ایمیل"
-        ></TextInput>
-        <View style={{ alignItems: "center", justifyContent: "center" }}>
-          <Text>موافق دریافت ایمیل هستم</Text>
-          <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={toggleSwitch}
-            value={isEnabled}
-          />
-          <Switch
-            trackColor={{ false: "#767577", true: "#81b0ff" }}
-            ios_backgroundColor="#3e3e3e"
-            value={props.result}
-          />
-        </View>
-        <Button
-          icon=""
-          mode="contained"
-          style={(styles.btn, { height: 40, margin: 5 })}
-          color="#841584"
-          // accessibilityLabel="ثبت نام"
-          onPress={() =>
-            props.register({ mobile: mobile, email: email, agree: isEnabled })
-          }
-        >
-          ثبت نام
-        </Button>
-      </View>
-      <Chart />
+      <View style={{flex:3,}} >
+         <TextInput
+           type="flat"
+           style={styles.inputStyle}
+           label="موبایل"
+           value={mobile}
+           onChangeText={(mobile) => setMobile(mobile)}
+           placeholder="شماره موبایل"
+           keyboardType={"numeric"}
+         ></TextInput>
+         <TextInput
+           type="flat"
+           style={styles.inputStyle}
+           label="ایمیل"
+           value={email}
+           onChangeText={(email) => setEmail(email)}
+           placeholder="ایمیل"
+         ></TextInput>
+         <View style={{ alignItems: "center", justifyContent: "center" }}>
+           <Text>موافق دریافت ایمیل هستم</Text>
+           <Switch
+             trackColor={{ false: "#767577", true: "#81b0ff" }}
+             thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+             ios_backgroundColor="#3e3e3e"
+             onValueChange={toggleSwitch}
+             value={isEnabled}
+           />
+           <Switch
+             trackColor={{ false: "#767577", true: "#81b0ff" }}
+             ios_backgroundColor="#3e3e3e"
+             value={props.result}
+           />
+         </View>
+         <Button
+           icon=""
+           mode="contained"
+           style={(styles.btn, { height: 40, margin: 5 })}
+           color="#841584"
+           // accessibilityLabel="ثبت نام"
+           onPress={() =>
+             props.register({ mobile: mobile, email: email, agree: isEnabled })
+           }
+         >
+           ثبت نام
+         </Button>
+     </View>
+
     </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  maincontainer: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  container: {
     flex: 1,
+      flexDirection: 'column',
+      alignItems: 'stretch',
   },
   innerdivs: {
     flex: 1,
     borderRadius: 5,
-    height: 130,
     padding: 10,
-    margin: 5,
+    height:"100%",
+    margin: 1,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -122,7 +130,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
     width: "100%",
     fontSize: 16,
-    padding: 5,
+    padding: 2,
   },
   btn: { flexDirection: "row" },
 });
---
+++ screens/production.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,TouchableHighlight
+  } from 'react-native';
+   
+  import Slick from 'react-native-slick';
+const Production=(props)=>{
+   
+      return (
+        <Slick style={styles.wrapper} showsButtons={true}>
+        <TouchableHighlight  onPress={() => {
+                     alert("قرعه کشی");
+                   }} style={styles.slide1}>
+                <Text style={styles.text}>قرعه کشی</Text>
+          </TouchableHighlight>
+          <TouchableHighlight  onPress={() => {
+                     alert("پیشنهاد ویژه");
+                   }} style={styles.slide2}>
+          <Text style={styles.text}>پیشنهاد ویژه</Text>
+          </TouchableHighlight>
+          <TouchableHighlight  onPress={() => {
+                     alert("سینما");
+                   }} style={styles.slide3}>
+          <Text style={styles.text}>سینما</Text>
+          </TouchableHighlight>
+        <View style={styles.slide4}>
+          <Text style={styles.text}>ورزشی</Text>
+        </View>
+      </Slick>
+      );
+
+};
+var styles = StyleSheet.create({
+    wrapper: {
+    },
+    slide1: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#e5dede',
+    },
+    slide2: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fcae8a',
+    },
+    slide3: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#ed4040',
+    },
+    slide4: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#53efef',
+      },
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
+    }
+  })
+
+
+export default Production;
